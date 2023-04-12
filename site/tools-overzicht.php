ec467d872cf83cd76d0b1d6c75f02aa0356d54a3
Bart Groothuysen <bart@groothuysen.com>
2023-04-12 10:40:33 +0200

    php toegevoegd en basis html structuur

---
+++ site/tools-overzicht.php
@@ -0,0 +1,29 @@
+<?php
+require 'database.php';
+
+//de sql query
+$sql = "SELECT * FROM tools";
+
+//hier wordt de query uitgevoerd met de database
+$result = mysqli_query($conn, $sql);
+
+$all_tools = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
