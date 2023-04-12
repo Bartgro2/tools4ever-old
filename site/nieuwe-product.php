30f7c7b39ccabfd58b447dc2cf324afa5ade19ba
Bart Groothuysen <bart@groothuysen.com>
2023-04-12 14:57:47 +0200

    nieuwe-product.php styling toegevoegd

---
+++ site/nieuwe-product.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+   <h1> Nieuw product</h1>
+   <form action="" method="post">
+
+      <div class="form-groep">
+         <label for="naamProduct">Naam product</label>
+         <input type="text" name="" id="naamProduct">
+      </div>
+      <div class="form-groep">
+         <label for="categorieProduct">Categorie product</label>
+         <input type="text" name="" id="categorieProduct">
+      </div>
+      <div class="form-groep">
+         <label for="prijsProduct">Prijs product</label>
+         <input type="text" name="" id="prijsProduct">
+      </div>
+      <div class="form-groep">
+         <label for="merkProduct">Merk product</label>
+         <input type="text" name="" id="merkProduct">
+      </div>
+      <button type="submit">maak nieuw product!</button>
+   </form>
+</body>
+
+</html>
