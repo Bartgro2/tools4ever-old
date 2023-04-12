6fa30afc3d5aad8520654ae5a78af12ad1464411
Bart Groothuysen <bart@groothuysen.com>
2023-04-12 15:32:22 +0200

    laatste wijzigingen

--- site/verwerk-nieuw-product.php
+++ site/verwerk-nieuw-product.php
@@ -1,14 +1,23 @@
-<?php 
+<?php
+
+if (!empty($_POST['naamProduct'])) {
 
 
-$naam       = $_POST['naamProduct'];
-$categorie  = $_POST['categorieProduct'];
-$prijs      = $_POST['prijsProduct'];
-$merk       = $_POST['merkProduct'];
+
+    $naam       = $_POST['naamProduct'];
+    $categorie  = $_POST['categorieProduct'];
+    $prijs      = $_POST['prijsProduct'];
+    $merk       = $_POST['merkProduct'];
 
 
-require 'database.php';
+    require 'database.php';
 
-$sql =  "INSERT INTO tools(tool_name, tool_category, tool_price, tool_brand) VALUES ('$naam','$categorie','$prijs','$merk')";
+    $sql =  "INSERT INTO tools(tool_name, tool_category, tool_price, tool_brand) VALUES ('$naam','$categorie','$prijs','$merk')";
 
-mysqli_query($conn,$sql);
+    mysqli_query($conn, $sql);
+
+    if (mysqli_query($conn, $sql)) {
+        header("location: tools-overzicht.php");
+        exit;
+    }
+}
