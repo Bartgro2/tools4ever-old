77cecb2e9f288b2979ffc09012a1b334cf28f2fb
Bart Groothuysen <bart@groothuysen.com>
2023-04-19 10:52:41 +0200

    form structuur nieuwe-gebruiker.php

--- site/nieuwe-gebruiker.php
+++ site/nieuwe-gebruiker.php
@@ -1,27 +1,44 @@
 <?php
- require 'database.php';
+require 'database.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
+
 <body>
     <div>
         <main>
-            <form action="">
-                <input type="text">
-                <input type="text">
-                <input type="email" name="" id="">
-                <input type="password" name="" id="">
-                <input type="text">
-                
+            <form action="" method="">
+
+                <label for="firstname">firstname</label>
+                <input type="text" name="firstname" id="firstname">
+                <label for="lastname">lastname</label>
+                <input type="text" name="lastname" id="lastname">
+
+                <label for="email">email</label>
+                <input type="email" name="email" id="email">
+                <label for="password">password</label>
+                <input type="password" name="password" id="password">
+                <label for="address">address</label>
+                <input type="text" name="address" id="address">
+                <label for="city">city</label>
+                <input type="text" name="city" id="city">
+                <select name="role" id="role">
+                    <option value="administrator">administrator</option>
+                    <option value="employee">employee</option>
+                    <option value="customer">customer</option>
+                </select>
             </form>
         </main>
     </div>
 </body>
+
 </html>
