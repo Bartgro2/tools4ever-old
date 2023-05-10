36800c33d56c08c0c5538d109137e9ea96a9dbf5
Bart Groothuysen <bart@groothuysen.com>
2023-05-10 10:47:41 +0200

    terugmaakt function login

---
+++ site/verwerk-inloggen.php
@@ -0,0 +1,22 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] !== "POST"){
+        header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+        include 'tools-overzicht.php';
+        exit;
+    }
+
+$email    = $_POST['email'];
+$password = $_POST['password'];
+
+require 'database.php';
+
+$sql = "SELECT * FROM users WHERE email = '$email'";
+
+$result = mysqli_query($conn,$sql);
+
+$user = mysqli_fetch_assoc($result);
+
+if(!is_array($user)){
+    header("location: inloggen.php");
+}
