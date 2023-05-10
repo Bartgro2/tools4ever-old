cc1292c5523a2ada318d122ebf473b71d78667a1
Bart Groothuysen <bart@groothuysen.com>
2023-05-10 14:53:49 +0200

    dashboards aangemaakt

--- site/verwerk-inloggen.php
+++ site/verwerk-inloggen.php
@@ -28,6 +28,24 @@ if ($user['password'] === $_POST['password']) {
 
     $_SESSION['isIngelogd'] = true;
     $_SESSION['voornaam'] = $user['firstname'];
+    $_SESSION['role'] = $user['role'];
+
+    switch ($user['role']) {
+        case 'administrator':
+            header('location: admin-dashboard.php');
+            exit();
+            break;
+
+        case 'employee':
+            header('location: employee-dashboard.php');
+            exit();
+            break;
+
+        default:
+            header('location: store.php');
+            exit();
+            break;
+    }
 
     header("location: dashboard.php");
     exit();
