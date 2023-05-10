0aa3a11d93647046f7c4fb6a3a40f6f2457ae15b
Bart Groothuysen <bart@groothuysen.com>
2023-05-10 11:06:44 +0200

    wachtwoord check (Inloggen)

--- site/verwerk-inloggen.php
+++ site/verwerk-inloggen.php
@@ -1,10 +1,10 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] !== "POST"){
-        header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
-        include 'tools-overzicht.php';
-        exit;
-    }
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+    include 'tools-overzicht.php';
+    exit;
+}
 
 $email    = $_POST['email'];
 $password = $_POST['password'];
@@ -13,10 +13,20 @@ require 'database.php';
 
 $sql = "SELECT * FROM users WHERE email = '$email'";
 
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 $user = mysqli_fetch_assoc($result);
 
-if(!is_array($user)){
+if (!is_array($user)) {
     header("location: inloggen.php");
+    exit();
 }
+
+if ($user['password'] === $_POST['password']) {
+
+    header("location: dashboard.php");
+    exit();
+}
+
+header("location: inloggen.php");
+exit();
