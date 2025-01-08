4aec16ede35264b8c90bc9ab94ca05f6b64b0523
Bart <akiraenishi@gmail.com>
2025-01-08 21:40:27 +0100

    fix

--- site/logout.php
+++ site/logout.php
@@ -6,15 +6,6 @@ unset($_SESSION);
 session_destroy();
 
 header("location: inloggen.php");
-exit;
-
-<?php
-
-session_start();
-
-unset($_SESSION);
-session_destroy();
-
-header("location: inloggen.php");
 exit();
+
 ?>
